c84984042ff75b59509d69e006aca94cc208734c
Ren Ichinose <kamatani.toru@gmail.com>
2023-04-15 21:00:06 +0900

    bjg_main.tsの実装

--- week1-2/blackjack-01/src/main.ts
+++ week1-2/blackjack-01/src/main.ts
@@ -1,14 +1,31 @@
+import { Card } from './classes/card/card';
+import { GameMaster } from './classes/gameMaster/gameMaster';
 import { Dealer } from './classes/person/dealer';
 import { Player } from './classes/person/player';
 
-/* eslint-disable-next-lin /*
+
 console.log('ブラックジャックを開始します。');
 
-/*
-プレイヤークラスとディーラークラスをインスタンス化する
-*/
-const player = new Player();
-const dealer = new Dealer();
+const crads = new Card();
+const dealer = new Dealer(crads);
+const player = new Player(crads);
+const gameMaster = new GameMaster(dealer, player);
 
-console.log(player.name);
-console.log(dealer.name);
+// プレイヤーとディーラーが各自2枚のトランプを取得する
+player.getRandomOne();
+player.getRandomOne();
+dealer.getRandomOne();
+dealer.getRandomOneSilent();
+
+gameMaster
+  .isNeedGetRandomOne() // プレイヤーがカードを引くターン
+  .then(() => {
+    console.log('ディーラーのターンを開始します。');
+    dealer.displayCards(); // プレイヤー画面に表示
+    gameMaster.getRandomrepeat(); // カードを繰り返し取得する
+    console.log('ディーラーのターンを終了します。');
+    gameMaster.displayWinner(); // 勝敗を発表する
+  })
+  .catch((error) => {
+    console.log(error.message); // エラー発生時の処理
+  });
