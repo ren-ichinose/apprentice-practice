c8ae076c322fbd68a46d684fdf71a8087626b473
Ren Ichinose <kamatani.toru@gmail.com>
2023-04-16 09:02:51 +0900

    bjg_step02_計算用関数の機能変更完了

--- week1-2/blackjack-01/src/utils/calculateCardScore.ts
+++ week1-2/blackjack-01/src/utils/calculateCardScore.ts
@@ -1,11 +1,89 @@
 import type { CardTypes } from '../classes/interfaces/interfaces';
 
 /*
-  カードの得点を計算する関数
-*/
+  カードの得点を計算する関数(ノーマル)
 export const calculateCardScore = (cards: CardTypes[]): number => {
-  const result = cards.reduce((sum, crad) => {
-    return sum + crad.number;
+  const result = cards.reduce((sum, card) => {
+    return sum + card.number;
   }, 0);
   return result;
 };
+*/
+
+
+// カードの得点を計算する関数(トランプの数字がAの場合、1もしくは11として計算する。)
+export const calculateCardScore = (cards: CardTypes[]): number => {
+  // トランプのAの配列を格納する変数
+  const aceCards: CardTypes[] = [];
+  let CardScore = 0;
+  /*
+    Aを除いた数字のスコアを計算
+    AはaceCardsに格納する。
+  */
+  const nonAceCardScore = cards.reduce((sum, card) => {
+    if (card.number !== 1) return sum + card.number;
+    aceCards.push(card);
+    return sum;
+  }, 0);
+
+  /*
+    Aを11として扱った場合、カードの合計の値が21を超えなければ、
+    Aを11として得点を計算し、呼び出しもとに得点の合計を返す。
+  */
+  // prettier-ignore
+  if (nonAceCardScore < (12 - aceCards.length)) {
+    CardScore = nonAceCardScore + 11 + (aceCards.length - 1)
+    return CardScore
+  }
+
+  /*
+    Aを1として得点を計算し、呼び出しもとに得点の合計を返す。
+  */
+  CardScore = nonAceCardScore + aceCards.length;
+  return CardScore;
+};
+
+// // 得点計算のテスト
+//   // 21
+//   console.log(
+//     calculateCardScore([
+//       { type: '', number: 10 },
+//       { type: '', number: 1 },
+//     ])
+//   );
+
+//   // 12
+//   console.log(
+//     calculateCardScore([
+//       { type: '', number: 11 },
+//       { type: '', number: 1 },
+//     ])
+//   );
+
+//   // 21
+//   console.log(
+//     calculateCardScore([
+//       { type: '', number: 9 },
+//       { type: '', number: 1 },
+//       { type: '', number: 1 },
+//     ])
+//   );
+
+//   // 12
+//   console.log(
+//     calculateCardScore([
+//       { type: '', number: 10 },
+//       { type: '', number: 1 },
+//       { type: '', number: 1 },
+//     ])
+//   );
+
+//   // 13
+//   console.log(
+//     calculateCardScore([
+//       { type: '', number: 10 },
+//       { type: '', number: 1 },
+//       { type: '', number: 1 },
+//       { type: '', number: 1 },
+//     ])
+//   );
