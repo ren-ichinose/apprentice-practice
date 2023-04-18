0725aeae26862ddf56a3915d24547772fca48ca5
Ren Ichinose <kamatani.toru@gmail.com>
2023-04-18 11:06:02 +0900

    bjg_得点計算のロジックの修正

--- week1-2/blackjack-01/src/utils/calculateCardScore.ts
+++ week1-2/blackjack-01/src/utils/calculateCardScore.ts
@@ -1,6 +1,5 @@
 import type { CardTypes } from '../classes/interfaces/interfaces';
 
-
 /*
   カードの得点を計算する関数(ノーマル)
 export const calculateCardScore = (cards: CardTypes[]): number => {
@@ -34,27 +33,27 @@ export const calculateCardScore = (cards: CardTypes[]): number => {
       Aを11として扱った場合、カードの合計の値が21を超えなければ、
       Aを11として得点を計算し、呼び出しもとに得点の合計を返す。
   */
-  let CardScore = 0;
   // prettier-ignore
   if (nonAceCardScore < (12 - aceCards.length)) {
-    CardScore = nonAceCardScore + 11 + (aceCards.length - 1)
+    const CardScore = nonAceCardScore + 11 + (aceCards.length - 1)
+    return CardScore
   }
 
   /*
     Aを1として得点を計算し、呼び出しもとに得点の合計を返す。
   */
-  CardScore = nonAceCardScore + aceCards.length;
+  const CardScore = nonAceCardScore + aceCards.length;
   return CardScore;
 };
 
 // // 得点計算のテスト
 //   // 21
-//   console.log(
-//     calculateCardScore([
-//       { type: '', number: 10 },
-//       { type: '', number: 1 },
-//     ])
-//   );
+// console.log(
+//   calculateCardScore([
+//     { type: '', number: 10 },
+//     { type: '', number: 1 },
+//   ])
+// );
 
 //   // 12
 //   console.log(
