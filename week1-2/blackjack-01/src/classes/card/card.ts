f05701d094d0b03e8b60d09e46e83f64599f1925
Ren Ichinose <kamatani.toru@gmail.com>
2023-04-20 16:13:57 +0900

    bjg_カードクラスののリファクタリング

--- week1-2/blackjack-01/src/classes/card/card.ts
+++ week1-2/blackjack-01/src/classes/card/card.ts
@@ -1,42 +1,40 @@
-import { deleteCard, findCrad } from '../../utils/cardsHandler';
 import type { CardTypes } from '../interfaces/interfaces';
-
 
 export class Card {
   /*
-    _currentCards: { type: string, number: number}[]
+    _currentDeckOfCards: { type: string, number: number}[]
     トランプの配列を格納するメンバ変数
   */
-  private _currentCards: CardTypes[];
+  private _currentDeckOfCards: CardTypes[];
 
   // 引数に応じた組み合わせのトランプを生成してセットする
   constructor(types: string[], jokerNumber: number) {
     const cards = this.createCards(types, jokerNumber);
-    this._currentCards = cards;
+    this._currentDeckOfCards = cards;
   }
 
-  get currentCards(): CardTypes[] {
-    return this._currentCards;
+  get currentDeckOfCards(): CardTypes[] {
+    return this._currentDeckOfCards;
   }
 
   // 52枚のトランプを生成する関数
   private createCards(types: string[], jokerNumber: number): CardTypes[] {
-    const mapCards: CardTypes[] = [];
+    const deckOfCardsWithoutJoker: CardTypes[] = [];
 
     types.forEach((type: string) => {
       [...Array(13)].forEach((_, i) => {
         const number = i + 1;
-        mapCards.push({ type, number });
+        deckOfCardsWithoutJoker.push({ type, number });
       });
     });
 
-    const jokers = this.createJoker(jokerNumber);
-    const resultCards = [...mapCards, ...jokers];
-    return resultCards;
+    const jokers = this.createJokers(jokerNumber);
+    const deckOfCards = [...deckOfCardsWithoutJoker, ...jokers];
+    return deckOfCards;
   }
 
   // 引数の値に応じて、ジョーカーを作成する
-  private createJoker(jokerNumber: number): CardTypes[] {
+  private createJokers(jokerNumber: number): CardTypes[] {
     const jokers: CardTypes[] = [];
     [...Array(jokerNumber)].forEach((_) => {
       jokers.push({ type: 'joker', number: 0 });
@@ -45,29 +43,28 @@ export class Card {
   }
 
   // ランダムに1枚のトランプを返す関数
-  getRandomOne(): CardTypes {
-    // ランダムにトランプを決定する
-    const randomIndex = Math.floor(Math.random() * this._currentCards.length);
-    const { type, number } = this._currentCards[randomIndex];
+  drawCardRandomOne(): CardTypes {
+    // ランダムにトランプを決定して取得する
+    const randomIndex = Math.floor(
+      Math.random() * this._currentDeckOfCards.length
+    );
+    const drawnCard = this._currentDeckOfCards[randomIndex];
 
-    // 選んだトランプを取得する
-    const randomCard = this.getSelectOne(type, number);
-    return randomCard;
+    // 取得したトランプを現在の手札から削除する
+    this.deleteCards(drawnCard);
+    return drawnCard;
   }
 
-  // 指定されたトランプを返す関数
-  getSelectOne(type: string, number: number): CardTypes {
-    // 現在の全てのトランプから、指定されたトランプを取得する
-    const findedCard = findCrad(type, number, this._currentCards);
-
-    // _currentCardsから取得したトランプを取り除いた配列を取得する
-    const newCurrentCards = deleteCard(
-      findedCard.type,
-      findedCard.number,
-      this._currentCards
+  // _currentDeckOfCardsから取得したトランプを1枚取り除く
+  deleteCards({ type, number }: CardTypes): void {
+    const newCurrentDeckOfCards = this.currentDeckOfCards.filter(
+      (currentCard) => {
+        if (currentCard.type === type && currentCard.number === number)
+          return false;
+        return true;
+      }
     );
 
-    this._currentCards = newCurrentCards;
-    return findedCard;
+    this._currentDeckOfCards = newCurrentDeckOfCards;
   }
 }
