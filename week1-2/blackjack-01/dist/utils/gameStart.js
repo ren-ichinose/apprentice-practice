5e54ca583119a89801d63dc922f703e0d67b5570
Ren Ichinose <kamatani.toru@gmail.com>
2023-04-20 17:34:03 +0900

    bjg_プレイヤー人数の誤入力の際の処理を変更

--- week1-2/blackjack-01/dist/utils/gameStart.js
+++ week1-2/blackjack-01/dist/utils/gameStart.js
@@ -6,12 +6,19 @@ const gmaeStartAndQuetion = async () => {
     console.log('ブラックジャックを開始します。');
     try {
         const totalComputerPlayer = await (0, questionYesOrNo_1.questionNumber)('CPUを追加する場合は0〜3の整数を入力してください。');
+        if (totalComputerPlayer > 3 || isNaN(totalComputerPlayer))
+            throw new Error();
         return totalComputerPlayer;
     }
     catch (error) {
-        console.log('入力内容に誤りがあります');
-        console.log('CPUは3人でゲームを開始します');
-        return 3;
+        console.log('Error:入力内容を確認してください。');
+        const totalComputerPlayer = await (0, questionYesOrNo_1.questionNumber)('CPUを追加する場合は0〜3の整数を入力してください。');
+        if (totalComputerPlayer > 3 || isNaN(totalComputerPlayer)) {
+            console.log('Error:入力内容を確認できませんでした。');
+            console.log('CPUは3人でゲームを開始します。');
+            return 3;
+        }
+        return totalComputerPlayer;
     }
 };
 exports.gmaeStartAndQuetion = gmaeStartAndQuetion;
