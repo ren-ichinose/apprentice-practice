91a766cbe5911a0c6e27c7c1e9c0fc6174bb1777
Ren Ichinose <kamatani.toru@gmail.com>
2023-04-22 09:15:00 +0900

    bjg_ゲーム開始時の画面表示内容の変更

--- week1-2/blackjack-01/src/main.ts
+++ week1-2/blackjack-01/src/main.ts
@@ -1,11 +1,17 @@
 import { GameMaster } from './classes/gameMaster/gameMaster';
 import { gmaeStartAndQuetion } from './utils/gameStart';
+import { questionYesOrNo } from './utils/questionYesOrNo';
 
 const blackJackGame = async (): Promise<void> => {
   // ゲームスタートの宣言と参加者の人数を標準入力より取得する
-  const totalPlayers = await gmaeStartAndQuetion();
-  const gameMaster = new GameMaster(totalPlayers);
-  await gameMaster.gameStart();
+  const isStart = await questionYesOrNo(
+    'ブラックジャックをはじめますか？（y/n）'
+  );
+  if (isStart) {
+    const totalPlayers = await gmaeStartAndQuetion();
+    const gameMaster = new GameMaster(totalPlayers);
+    await gameMaster.gameStart();
+  }
 };
 
 blackJackGame().catch(() => {
