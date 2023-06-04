36b238a79635fa0720694e3132ea78c0a6cfab51
Ren Ichinose <kamatani.toru@gmail.com>
2023-06-04 22:39:16 +0900

    quest3_setup_prisma

---
+++ week9-10/quest_3/prisma/migrations/20230604132928_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "todos" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "todos_pkey" PRIMARY KEY ("id")
+);
