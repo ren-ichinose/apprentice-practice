f01f9c9a222dd931886ff5e62ba3b9c3c7be7f93
Ren Ichinose <kamatani.toru@gmail.com>
2023-06-05 13:25:29 +0900

    quest3_add_prisma_moduleとserviceの実装

--- week9-10/quest_3/src/app.module.ts
+++ week9-10/quest_3/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import PrismaModule from './prisma/prisma.module';
+import TodoModule from './todo/todo.module';
 import AppController from './app.controller';
 import AppService from './app.service';
 
 @Module({
-  imports: [],
+  imports: [TodoModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
